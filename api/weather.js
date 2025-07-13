26d83aeaab3fa587675b61ae4392ab045c3d0d7c
gregormarc7 <gregor.marc7@gmail.com>
2025-07-13 15:20:53 +0200

    Update weather.js

--- api/weather.js
+++ api/weather.js
@@ -16,9 +16,9 @@ exports.handler = async function(event, context) {
 
     const tempZrak = data.main.temp;
     const veter = data.wind.speed;
-    const ikona = data.weather[0].icon; // npr. 01d
-    const ikonaUrl = `https://openweathermap.org/img/wn/${ikona}@2x.png`;
 
+    const iconCode = data.weather[0].icon; // npr. "01d"
+    const ikonaUrl = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
 
     return {
       statusCode: 200,
@@ -26,8 +26,9 @@ exports.handler = async function(event, context) {
         "Access-Control-Allow-Origin": "*",
         "Content-Type": "application/json"
       },
-       body: JSON.stringify({ tempZrak, veter, ikonaUrl })
+      body: JSON.stringify({ tempZrak, veter, ikonaUrl }) // <- KLJUČNO
     };
+
   } catch (error) {
     return {
       statusCode: 500,
