acda986d4646dc803410766dd8454150a3a6fc05
gregormarc7 <gregor.marc7@gmail.com>
2025-07-20 17:02:00 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,30 @@
 import Head from 'next/head'
 import Hero from '../components/Hero'
+import Weather from '../components/Weather'
+import SectionIzola from '../components/SectionIzola'
+import SectionApartma from '../components/SectionApartma'
+import SectionPovprasevanje from '../components/SectionPovprasevanje'
+import SectionLokacija from '../components/SectionLokacija'
+import Header from '../components/Header'
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Studio Aurora</title>
+        <title>Studio Aurora – Izola</title>
         <meta name="description" content="Sodoben apartma v Izoli – Studio Aurora" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Hero />
+      <Header />
+      <main>
+        <Weather />
+        <Hero />
+        <SectionIzola />
+        <SectionApartma />
+        <SectionPovprasevanje />
+        <SectionLokacija />
+      </main>
     </>
   )
 }
