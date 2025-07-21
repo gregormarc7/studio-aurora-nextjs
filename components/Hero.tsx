d7e97e0af65ee9ffc4e34b1e67ec0e6e80d42270
gregormarc7 <gregor.marc7@gmail.com>
2025-07-21 13:03:15 +0200

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,7 +4,7 @@ export default function Hero() {
       id="domov"
       className="relative h-screen flex items-center justify-center text-white text-center px-6"
       style={{
-        backgroundImage: `url('/images/izola-hero.png')`,
+        backgroundImage: `url('/images/cover-izola.jpg')`,
         backgroundSize: 'cover',
         backgroundPosition: 'center',
       }}
