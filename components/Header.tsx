6e2b414d656b106bb4e7a8abb0a5aa2a4c44bd54
gregormarc7 <gregor.marc7@gmail.com>
2025-07-21 11:03:47 +0200

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -22,8 +22,8 @@ export default function Header() {
           <Image
             src="/images/logo.png"
             alt="Studio Aurora logo"
-            width={130}
-            height={130}
+            width={100}
+            height={100}
             className="w-40 h-40 object-contain"
           />
         </div>
